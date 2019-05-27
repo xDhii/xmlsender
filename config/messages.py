671100b18545451b571ec76809df2c1708d244d3
Leozera011 <leonardo.carvalho1208@gmail.com>
2019-05-27 23:48:47 +0000

    alteração do titulo

--- config/messages.py
+++ config/messages.py
@@ -6,9 +6,8 @@ def limpar_tela():
 
 def mensageminicial():
     print()
-    print("\033[1;30;47m        Criador de IDOC v0.3.10 (beta)        ")
+    print("\033[1;30;47m        Criador de XML        ")
     print()
-    print('\33[1;37;40mEstá faltando alguma empresa ou tipo de documento?')
     print('\33[1;37;40mMe avise: adriano.valumin@sovos.com')
     print('\033[0m')
     print()
